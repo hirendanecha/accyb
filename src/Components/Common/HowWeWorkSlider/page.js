c129a4e5ff430aea0d9bffe8478ebd46f12ce763
pratik-opash <pra.opash@gmail.com>
2024-03-06 14:27:46 +0530

    Notre fonctionnement

--- src/Components/Common/HowWeWorkSlider/page.js
+++ src/Components/Common/HowWeWorkSlider/page.js
@@ -20,7 +20,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 // import required modules
-import { Keyboard, Navigation, Pagination } from "swiper/modules";
+import { Autoplay, Keyboard, Navigation, Pagination } from "swiper/modules";
 import { inter } from "../../../fonts/fonts";
 const reviews = [
   {
@@ -119,6 +119,150 @@ const reviews = [
     position: "Marketing Coordinator",
     // img: Client6Img,
   },
+  {
+    rating: 5,
+    review:
+      "We Chose Squareshot because it’s the perfect combination of convenienc.",
+    reviewer: "Ronald Richards",
+    position: "President of Sales",
+    // img: Client4Img,
+  },
+  {
+    rating: 5,
+    review:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.",
+    reviewer: "Dianne Russell",
+    position: "Web Designer",
+    // img: Client5Img,
+  },
+  {
+    rating: 5,
+    review:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.",
+    reviewer: "Eleanor Pena",
+    position: "Marketing Coordinator",
+    // img: Client6Img,
+  },
+  {
+    rating: 5,
+    review:
+      "We Chose Squareshot because it’s the perfect combination of convenienc.",
+    reviewer: "Ronald Richards",
+    position: "President of Sales",
+    // img: Client4Img,
+  },
+  {
+    rating: 5,
+    review:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.",
+    reviewer: "Dianne Russell",
+    position: "Web Designer",
+    // img: Client5Img,
+  },
+  {
+    rating: 5,
+    review:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.",
+    reviewer: "Eleanor Pena",
+    position: "Marketing Coordinator",
+    // img: Client6Img,
+  },
+  {
+    rating: 5,
+    review:
+      "We Chose Squareshot because it’s the perfect combination of convenienc.",
+    reviewer: "Ronald Richards",
+    position: "President of Sales",
+    // img: Client4Img,
+  },
+  {
+    rating: 5,
+    review:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.",
+    reviewer: "Dianne Russell",
+    position: "Web Designer",
+    // img: Client5Img,
+  },
+  {
+    rating: 5,
+    review:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.",
+    reviewer: "Eleanor Pena",
+    position: "Marketing Coordinator",
+    // img: Client6Img,
+  },
+  {
+    rating: 5,
+    review:
+      "We Chose Squareshot because it’s the perfect combination of convenienc.",
+    reviewer: "Ronald Richards",
+    position: "President of Sales",
+    // img: Client4Img,
+  },
+  {
+    rating: 5,
+    review:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.",
+    reviewer: "Dianne Russell",
+    position: "Web Designer",
+    // img: Client5Img,
+  },
+  {
+    rating: 5,
+    review:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.",
+    reviewer: "Eleanor Pena",
+    position: "Marketing Coordinator",
+    // img: Client6Img,
+  },
+  {
+    rating: 5,
+    review:
+      "We Chose Squareshot because it’s the perfect combination of convenienc.",
+    reviewer: "Ronald Richards",
+    position: "President of Sales",
+    // img: Client4Img,
+  },
+  {
+    rating: 5,
+    review:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.",
+    reviewer: "Dianne Russell",
+    position: "Web Designer",
+    // img: Client5Img,
+  },
+  {
+    rating: 5,
+    review:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.",
+    reviewer: "Eleanor Pena",
+    position: "Marketing Coordinator",
+    // img: Client6Img,
+  },
+  {
+    rating: 5,
+    review:
+      "We Chose Squareshot because it’s the perfect combination of convenienc.",
+    reviewer: "Ronald Richards",
+    position: "President of Sales",
+    // img: Client4Img,
+  },
+  {
+    rating: 5,
+    review:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.",
+    reviewer: "Dianne Russell",
+    position: "Web Designer",
+    // img: Client5Img,
+  },
+  {
+    rating: 5,
+    review:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.",
+    reviewer: "Eleanor Pena",
+    position: "Marketing Coordinator",
+    // img: Client6Img,
+  },
 ];
 
 // Split Reviews in chunks of [{}, {}]
@@ -135,94 +279,16 @@ export default function HowToWorkSlider() {
         pt: { xs: 10, sm: 10, lg: 15, xl: 20 },
       }}
     >
-      <Container disableGutters maxWidth={"xl"}>
-        <Grid
-          container
-          columnSpacing={2}
-          rowSpacing={6}
-          sx={{ display: "flex", alignItems: "center" }}
-        >
-          <Grid item xs={12} md={2.5}>
-            <Box
-              sx={{
-                display: { md: "flex", xs: "none" },
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <IconButton
-                className="swiper-button-prev-testimonials"
-                aria-label="Next Slide"
-                title="Next Slide"
-                sx={{ bgcolor: "#007A47", width: 45, height: 45 }}
-              >
-                <ArrowBackIcon sx={{ color: "#FFFFFF" }} />
-              </IconButton>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={7}>
-            <Typography
-              sx={{
-                justifyContent: "center",
-                display: "flex",
-                fontFamily: inter.style.fontFamily,
-              }}
-            >
-              Notre fonctionnement
-            </Typography>
-            <Typography
-              variant="h2"
-              color="#007A47"
-              sx={{
-                textTransform: "capitalize",
-                display: "flex",
-                justifyContent: "center",
-                textAlign: "center",
-                mt: 3,
-                fontFamily: inter.style.fontFamily,
-                fontSize: { lg: "64px", md: "50px", xs: "40px" },
-              }}
-            >
-              Conseil d’administration
-            </Typography>
-            <Typography
-              sx={{
-                fontFamily: inter.style.fontFamily,
-                justifyContent: "center",
-                display: "flex",
-                textAlign: "center",
-                mt: 3,
-              }}
-            >
-              Le conseil d’administration détermine les axes stratégiques et les
-              orientations de l’activité de l’association ainsi que les grands
-              principes de fonctionnement de l’Association et veille à leur mise
-              en œuvre.
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={2.5}>
-            <Box
-              className="swiper-button-next-testimonials"
-              sx={{
-                display: { md: "flex", xs: "none" },
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <IconButton
-                aria-label="Next Slide"
-                title="Next Slide"
-                sx={{ bgcolor: "#007A47", width: 45, height: 45 }}
-              >
-                <ArrowForwardIcon sx={{ color: "#FFFFFF" }} />
-              </IconButton>
-            </Box>
-          </Grid>
-        </Grid>
-        <Box sx={{ display: "flex", justifyContent: "center", gap: 3 }}>
+      <Grid
+        container
+        columnSpacing={2}
+        rowSpacing={6}
+        sx={{ display: "flex", alignItems: "center" }}
+      >
+        <Grid item xs={12} md={2.5}>
           <Box
             sx={{
-              display: { md: "none", xs: "block" },
+              display: { md: "flex", xs: "none" },
               justifyContent: "center",
               alignItems: "center",
             }}
@@ -236,10 +302,52 @@ export default function HowToWorkSlider() {
               <ArrowBackIcon sx={{ color: "#FFFFFF" }} />
             </IconButton>
           </Box>
+        </Grid>
+        <Grid item xs={12} md={7}>
+          <Typography
+            sx={{
+              justifyContent: "center",
+              display: "flex",
+              fontFamily: inter.style.fontFamily,
+            }}
+          >
+            Notre fonctionnement
+          </Typography>
+          <Typography
+            variant="h2"
+            color="#007A47"
+            sx={{
+              textTransform: "capitalize",
+              display: "flex",
+              justifyContent: "center",
+              textAlign: "center",
+              mt: 3,
+              fontFamily: inter.style.fontFamily,
+              fontSize: { lg: "64px", md: "50px", xs: "40px" },
+            }}
+          >
+            Conseil d’administration
+          </Typography>
+          <Typography
+            sx={{
+              fontFamily: inter.style.fontFamily,
+              justifyContent: "center",
+              display: "flex",
+              textAlign: "center",
+              mt: 3,
+            }}
+          >
+            Le conseil d’administration détermine les axes stratégiques et les
+            orientations de l’activité de l’association ainsi que les grands
+            principes de fonctionnement de l’Association et veille à leur mise
+            en œuvre.
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={2.5}>
           <Box
             className="swiper-button-next-testimonials"
             sx={{
-              display: { md: "none", xs: "block" },
+              display: { md: "flex", xs: "none" },
               justifyContent: "center",
               alignItems: "center",
             }}
@@ -252,38 +360,76 @@ export default function HowToWorkSlider() {
               <ArrowForwardIcon sx={{ color: "#FFFFFF" }} />
             </IconButton>
           </Box>
-          <Box></Box>
-        </Box>
+        </Grid>
+      </Grid>
+      <Box sx={{ display: "flex", justifyContent: "center", gap: 3 }}>
         <Box
-          component={Swiper}
-          keyboard={true}
-          modules={[Navigation, Keyboard, Pagination]}
-          slidesPerView={1}
-          spaceBetween={24}
-          navigation={{
-            nextEl: ".swiper-button-next-testimonials",
-            prevEl: ".swiper-button-prev-testimonials",
-            enabled: true,
-          }}
-          breakpoints={{
-            600: { slidesPerView: 2 },
-            1200: { slidesPerView: 5 },
-          }}
-          mt={12}
           sx={{
-            "& .swiper-pagination": { position: "static", mt: 16 },
-            "& .swiper-pagination-bullet": {
-              bgcolor: "#5314A9",
-              width: 4,
-              height: 4,
-              mx: "0.5rem !important",
-            },
+            display: { md: "none", xs: "block" },
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          {list?.map((chunks, i) => (
-            <SwiperSlide key={150 + i}>
-              <Grid container spacing={3}>
-                {chunks.map((review, j) => (
+          <IconButton
+            className="swiper-button-prev-testimonials"
+            aria-label="Next Slide"
+            title="Next Slide"
+            sx={{ bgcolor: "#007A47", width: 45, height: 45 }}
+          >
+            <ArrowBackIcon sx={{ color: "#FFFFFF" }} />
+          </IconButton>
+        </Box>
+        <Box
+          className="swiper-button-next-testimonials"
+          sx={{
+            display: { md: "none", xs: "block" },
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <IconButton
+            aria-label="Next Slide"
+            title="Next Slide"
+            sx={{ bgcolor: "#007A47", width: 45, height: 45 }}
+          >
+            <ArrowForwardIcon sx={{ color: "#FFFFFF" }} />
+          </IconButton>
+        </Box>
+        <Box></Box>
+      </Box>
+      <Box
+        component={Swiper}
+        keyboard={true}
+        modules={[Navigation, Keyboard, Pagination, Autoplay]}
+        slidesPerView={1}
+        spaceBetween={24}
+        loop={true}
+        navigation={{
+          nextEl: ".swiper-button-next-testimonials",
+          prevEl: ".swiper-button-prev-testimonials",
+          enabled: true,
+        }}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        breakpoints={{
+          600: { slidesPerView: 2 },
+          1200: { slidesPerView: 5 },
+        }}
+        mt={12}
+        sx={{
+          "& .swiper-pagination": { position: "static", mt: 16 },
+          "& .swiper-pagination-bullet": {
+            bgcolor: "#5314A9",
+            width: 4,
+            height: 4,
+            mx: "0.5rem !important",
+          },
+        }}
+      >
+        {list?.map((chunks, i) => (
+          <SwiperSlide key={150 + i}>
+            <Grid container spacing={3}>
+              {chunks.map((review, j) => {
+                return (
                   <Grid item xs={12} sm={6} md={12} key={j + 256}>
                     <Box
                       sx={{
@@ -291,6 +437,7 @@ export default function HowToWorkSlider() {
                         border: "1px solid black",
                         padding: 5,
                         borderRadius: "10px",
+                        transform: j === 0 ? "" : "translateX(-50%)",
                       }}
                     >
                       <Typography
@@ -309,12 +456,12 @@ export default function HowToWorkSlider() {
                       </Typography>
                     </Box>
                   </Grid>
-                ))}
-              </Grid>
-            </SwiperSlide>
-          ))}
-        </Box>
-      </Container>
+                );
+              })}
+            </Grid>
+          </SwiperSlide>
+        ))}
+      </Box>
     </Box>
   );
 }
