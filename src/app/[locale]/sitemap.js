8fd34e0863e820a7b96ef5a730d367aeb01e0aa3
pratik-opash <pra.opash@gmail.com>
2024-03-07 17:40:27 +0530

    add site map again

---
+++ src/app/[locale]/sitemap.js
@@ -0,0 +1,21 @@
+const WEBSITE_HOST_URL = "https://accyb.vercel.app";
+const changeFrequency =
+  "always" | "hourly" | "daily" | "weekly" | "monthly" | "yearly" | "never";
+export default function sitemap() {
+  const changeFrequency = "daily";
+  const routes = [
+    "",
+    "/devenirCyber",
+    "/discoverTheACCYB",
+    "/discoverTheObservatory",
+    "/FindOutAtlantic",
+    "/news",
+    "/seFormer",
+    "/sInformer",
+  ].map((route) => ({
+    url: `${WEBSITE_HOST_URL}${route}`,
+    lastModified: new Date().toISOString(),
+    changeFrequency,
+  }));
+  return [...routes];
+}
