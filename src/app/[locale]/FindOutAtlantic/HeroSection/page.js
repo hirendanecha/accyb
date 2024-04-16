ce456efde731eaaeb1ea8bc815b24197df7be3dc
pratik-raval <pratikcraval@gmail.com>
2024-04-16 12:51:08 +0530

    add logo

--- src/app/[locale]/FindOutAtlantic/HeroSection/page.js
+++ src/app/[locale]/FindOutAtlantic/HeroSection/page.js
@@ -1,9 +1,19 @@
 "use client";
-import { Box, Button, Container, Grid, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Typography, styled } from "@mui/material";
 import React from "react";
 import { inter } from "../../../../fonts/fonts";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import Image from "next/image";
+import Logo from "../../../../Icons/Logo.svg";
+const Img = styled(Image)(({ theme }) => ({
+  width: "151px !important",
+  height: "151px !important",
+  [theme.breakpoints.down("sm")]: {
+    width: "100px !important",
+    height: "100px !important",
+  },
+}));
 
 export default function HeroSection() {
   console.log("done");
@@ -21,7 +31,7 @@ export default function HeroSection() {
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         backgroundPosition: "center",
-        height: { md: "770px", xs: "690px" },
+        height: { md: "770px", xs: "800px" },
         width: "100%",
       }}
     >
@@ -31,12 +41,12 @@ export default function HeroSection() {
           justifyContent: "center",
           alignItems: "center",
           height: "100%",
-          marginTop: { md: 10, xs: 5 },
+          marginTop: { md: 10, xs: 6 },
         }}
       >
         <Container disableGutters maxWidth={"xl"}>
           <Grid container display={"flex"} justifyContent={"space-between"}>
-            <Grid item xs={12} lg={6} sx={{ padding: "0 16px" }}>
+            <Grid item xs={12} md={6} sx={{ padding: "0 16px" }}>
               <Typography
                 sx={{
                   fontFamily: inter.style.fontFamily,
@@ -175,81 +185,85 @@ export default function HeroSection() {
                 </Button>
               </Box>
             </Grid>
+            <Grid item md={2}></Grid>
             <Grid
               item
               xs={12}
-              lg={6}
+              md={4}
               sx={{
                 display: "flex",
+                alignItems: "center",
                 maxWidth: "550px !important",
-                padding: "0 16px",
                 position: "relative",
+                padding: "0 16px",
               }}
             >
-              <Box mt={5}>
+              <Box mt={{ md: -15, xs: 5 }}>
+                <Box sx={{ display: "flex", justifyContent: { lg: "flex-end", xs: "center" } }}>
+                  <Img src={Logo} alt="logo" />
+                </Box>
+                <Typography
+                  sx={{
+                    mt: { md: 5, xs: 3 },
+                    fontFamily: inter.style.fontFamily,
+                    color: "#FFFFFF",
+                    fontSize: { md: "14px", xs: "12px" },
+                    lineHeight: { md: "22px", xs: "20px" },
+                  }}
+                >
+                  Le CSIRT-ATLANTIC est le centre d'assistance cyber unique pour les bénéficiaires de taille
+                  intermédiaire des territoires français d'Amérique. Il est au cœur du dispositif national de CSIRT
+                  régionaux soutenu par l'ANSSI, engagé dans des actions de prévention, protection et partage
+                  d'information communautaire cyber.
+                </Typography>
                 <Typography
                   sx={{
                     fontFamily: inter.style.fontFamily,
                     color: "#FFFFFF",
-                    mt: { lg: 10, xs: 0 },
-                    fontSize: { md: "15px", xs: "13px" },
-                    lineHeight: { md: "25px", xs: "22px" },
+                    mt: 2,
+                    fontSize: { md: "14px", xs: "12px" },
+                    lineHeight: { md: "22px", xs: "20px" },
                   }}
                 >
-                  Un Computer Security incident Response Team est un centre de réponse aux incidents de sécurité.
+                  Horaires d'ouverture :
+                  <br />
+                  Lundi au vendredi (hors jours fériés) de 8h30 à 12h30 - 14h00 à 17h00
                 </Typography>
-                <Button
-                  variant="outlined"
-                  sx={{
-                    fontFamily: inter.style.fontFamily,
-                    mt: { lg: 5, xs: 3 },
-                    color: "#FFFFFF",
-                    border: "1px solid #D3D5DD",
-                    borderRadius: "61px",
-                    padding: { md: "18px 40px", xs: "18px 16px" },
-                    fontSize: { md: "12px", xs: "10px" },
-                    fontWeight: 600,
-                    ":hover": {
-                      borderColor: "white",
-                    },
-                  }}
-                >
-                  {`Horaires d'ouverture : Lundi au vendredi de 09h00 à 18h00`}
-                </Button>
-                <Box
-                  sx={{
-                    display: { lg: "flex", xs: "none" },
-                    gap: 2,
-                    cursor: "pointer",
-                    alignItems: "center",
-                    position: "absolute",
-                    bottom: 0,
-                    right: 30,
-                    top: 400,
-                  }}
-                >
-                  <Box onClick={handleContactUsClick}>
-                    <Typography
-                      sx={{
-                        color: "#FFFFFF",
-                        fontSize: "20px",
-                        fontWeight: 400,
-                        fontFamily: inter.style.fontFamily,
-                      }}
-                    >
-                      Scroll
-                    </Typography>
-                  </Box>
-
-                  <ArrowDownwardIcon sx={{ color: "#FFFFFF" }} />
+              </Box>
+              <Box
+                sx={{
+                  display: { lg: "flex", xs: "none" },
+                  gap: 2,
+                  cursor: "pointer",
+                  alignItems: "center",
+                  position: "absolute",
+                  right: 10,
+                  bottom: -50,
+                }}
+              >
+                <Box onClick={handleContactUsClick}>
+                  <Typography
+                    sx={{
+                      color: "#FFFFFF",
+                      fontSize: "20px",
+                      fontWeight: 400,
+                      fontFamily: inter.style.fontFamily,
+                    }}
+                  >
+                    Scroll
+                  </Typography>
                 </Box>
+
+                <ArrowDownwardIcon sx={{ color: "#FFFFFF" }} />
               </Box>
             </Grid>
             <Box
               sx={{
                 display: { md: "none", xs: "block" },
+                flexDirection: { md: "row", xs: "column" },
                 gap: { md: 5, xs: 1 },
                 mt: 5,
+                ml: 1,
                 maxWidth: { md: "unset", xs: "320px" },
               }}
             >
@@ -332,8 +346,8 @@ export default function HeroSection() {
                   border: "1px solid rgba(142, 155, 191, 0.3)",
                   borderRadius: "61px",
                   fontSize: "12px",
+                  mt: 2,
                   padding: "8px 30px",
-                  mt: 1,
                   fontWeight: 600,
                   background: "rgba(255, 255, 255, 0.1)",
                   fontFamily: inter.style.fontFamily,
