2d290de8298d6d2896abe006bba871f1d36d1ed3
abhi-opash <apa.opash@gmail.com>
2024-07-12 11:16:18 +0530

    Set OG Image

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -5,7 +5,7 @@ import { inter } from "../../fonts/fonts";
 import { NextIntlClientProvider, useMessages } from "next-intl";
 import ThemeProviderWrapper from "../../fonts/ThemeProviderWrapper";
 import ProviderLayout from "../provider";
-import Cookie from '../[locale]/cookie';
+import Cookie from "../[locale]/cookie";
 export const metadata = {
   title: "ACCYB",
   description: "Caribbean Cybersecurity Agency (CCYBA)",
@@ -13,9 +13,20 @@ export const metadata = {
 
 export default function RootLayout({ children, params: { locale } }) {
   const messages = useMessages();
+
   return (
     <ThemeProviderWrapper>
       <html lang={locale}>
+        <head>
+          <meta property="og:title" content="ACCYB" />
+          <meta
+            property="og:description"
+            content="Caribbean Cybersecurity Agency (CCYBA)"
+          />
+          <meta property="og:image" content="https://www.accyb.org/Thumbnail.svg" />
+          <meta property="og:url" content="https://www.accyb.org" />
+          <meta property="og:type" content="website" />
+        </head>
         <body className={inter.className}>
           <ProviderLayout>
             <NextIntlClientProvider locale={locale} messages={messages}>
